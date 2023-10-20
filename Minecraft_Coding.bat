24e1c82f81385f4deefbd021c2fb38b8729bdf90
Vegz78 <49032025+Vegz78@users.noreply.github.com>
2023-10-20 02:39:18 +0200

    Add files via upload

---
+++ Minecraft_Coding.bat
@@ -0,0 +1,5 @@
+start cmd /C "C:\Program Files\CodeConnectFix\CodeConnectFix.exe"
+sleep 2
+start shell:AppsFolder\Microsoft.MEECC_8wekyb3d8bbwe!CodeConnection
+start shell:AppsFolder\Microsoft.MinecraftUWP_8wekyb3d8bbwe!App
+echo /connect localhost:19135 | clip
